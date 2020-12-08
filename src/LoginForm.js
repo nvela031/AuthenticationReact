01362e2e751338a4fa3cb3ce74d238150acd8cc4
nvela031 <nvela031@fiu.edu>
2020-12-08 12:17:20 -0500

    setting up the logIn Firebase

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,14 +1,61 @@
 import React, {Component} from 'react';
-import {ButtonSection, Card, CardSection, Input} from './components/common';
+import {Text} from 'react-native';
+import {
+  ButtonSection,
+  Card,
+  CardSection,
+  Input,
+  Spinner,
+} from './components/common';
 import firebase from 'firebase';
 
 export class LoginForm extends Component {
-  state = {email: '', password: ''};
+  state = {email: '', password: '', error: '', loading: false};
 
   onButtonPress() {
     const {email, password} = this.state;
 
-    firebase.auth().signInWithEmailAndPassword(email, password);
+    this.setState({error: '', loading: true});
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authencation Failed',
+      loading: false,
+    });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '',
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <ButtonSection onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </ButtonSection>
+    );
   }
 
   render() {
@@ -33,14 +80,19 @@ export class LoginForm extends Component {
           />
         </CardSection>
 
-        <CardSection>
-          <ButtonSection onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </ButtonSection>
-        </CardSection>
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
+
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
 }
 
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+};
 export default LoginForm;
